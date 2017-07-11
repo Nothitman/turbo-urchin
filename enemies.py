9d24e051410fa471320bcad454ed8c5c65423c90
Nothitman <nothitman@gmail.com>
2017-07-11 19:23:39 +0100

    Added Boss & World Loops

--- enemies.py
+++ enemies.py
@@ -14,3 +14,7 @@ class GiantSpider(Enemy):
 class Ogre(Enemy):
     def __init__(self):
         super().__init__(name="Ogre", hp="30", damage="15")
+
+class Boss(Enemy):
+    def __init__(self):
+        super().__init__(name="Boss", hp="60", damage="22")
